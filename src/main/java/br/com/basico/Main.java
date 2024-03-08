d918e6d6795bffbbc0b9fce8b76b8c4710a63364
andre souza <andssouzza@gmail.com>
2024-03-07 21:12:46 -0400

    first commit

---
+++ src/main/java/br/com/basico/Main.java
@@ -0,0 +1,15 @@
+package br.com.basico;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+       String nome = "Andre Costa de Souza";
+
+       for(char letra : nome.toCharArray()) {
+           System.out.println(letra);
+       }
+
+    }
+}
